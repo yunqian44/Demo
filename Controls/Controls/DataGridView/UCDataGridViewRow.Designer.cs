ded81d16d92fcbe936ccc19b35b9e4f2dd882710
yunqian007 <yunqian@huodada.com>
2019-09-10 18:08:41 +0800

    新增自定义控件

---
+++ Controls/Controls/DataGridView/UCDataGridViewRow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Controls.Controls.DataGridView
+{
+    partial class UCDataGridViewRow
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panCells = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // panCells
+            // 
+            this.panCells.ColumnCount = 1;
+            this.panCells.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panCells.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.panCells.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panCells.Location = new System.Drawing.Point(0, 0);
+            this.panCells.Name = "panCells";
+            this.panCells.RowCount = 1;
+            this.panCells.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.panCells.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.panCells.Size = new System.Drawing.Size(554, 54);
+            this.panCells.TabIndex = 0;
+            // 
+            // UCDataGridViewRow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panCells);
+            this.Name = "UCDataGridViewRow";
+            this.Size = new System.Drawing.Size(554, 54);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panCells;
+    }
+}
---
+++ Controls/Controls/DataGridView/UCDataGridViewRow.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Controls.DataGridView;
+
+namespace Controls.Controls.DataGridView
+{
+    public partial class UCDataGridViewRow : UserControl,IDataGridViewRow
+    {
+        #region 属性
+        /// <summary>
+        /// CheckBox选中事件
+        /// </summary>
+        public event DataGridViewEventHandler CheckBoxChangeEvent;
+        /// <summary>
+        /// Occurs when [row custom event].
+        /// </summary>
+        public event DataGridViewRowCustomEventHandler RowCustomEvent;
+        /// <summary>
+        /// 点击单元格事件
+        /// </summary>
+        public event DataGridViewEventHandler CellClick;
+
+        /// <summary>
+        /// 数据源改变事件
+        /// </summary>
+        public event DataGridViewEventHandler SourceChanged;
+
+        /// <summary>
+        /// 列参数，用于创建列数和宽度
+        /// </summary>
+        /// <value>The columns.</value>
+        public List<DataGridViewColumnEntity> Columns
+        {
+            get;
+            set;
+        }
+
+        public object DataSource
+        {
+            get;
+            set;
+        }
+
+        public bool IsShowCheckBox
+        {
+            get;
+            set;
+        }
+
+        private bool m_isChecked;
+
+        public bool IsChecked
+        {
+            get
+            {
+                return m_isChecked;
+            }
+
+            set
+            {
+                if (m_isChecked != value)
+                {
+                    m_isChecked = value;
+                    (this.panCells.Controls.Find("check", false)[0] as CheckBox).Checked = value;
+                }
+            }
+        }
+        /// <summary>
+        /// The m row height
+        /// </summary>
+        int m_rowHeight = 40;
+        /// <summary>
+        /// 行高
+        /// </summary>
+        /// <value>The height of the row.</value>
+        public int RowHeight
+        {
+            get
+            {
+                return m_rowHeight;
+            }
+            set
+            {
+                m_rowHeight = value;
+                this.Height = value;
+            }
+        }
+
+        #endregion
+        public UCDataGridViewRow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UCDataGridViewRow" /> class.
+        /// </summary>
+        public UCDataGridViewRow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 绑定数据到Cell
+        /// </summary>
+        /// <returns>返回true则表示已处理过，否则将进行默认绑定（通常只针对有Text值的控件）</returns>
+        public void BindingCellData()
+        {
+            for (int i = 0; i < Columns.Count; i++)
+            {
+                DataGridViewColumnEntity com = Columns[i];
+                var cs = this.panCells.Controls.Find("lbl_" + com.DataField, false);
+                if (cs != null && cs.Length > 0)
+                {
+                    var pro = DataSource.GetType().GetProperty(com.DataField);
+                    if (pro != null)
+                    {
+                        var value = pro.GetValue(DataSource, null);
+                        if (com.Format != null)
+                        {
+                            cs[0].Text = com.Format(value);
+                        }
+                        else
+                        {
+                            cs[0].Text = value.ToStringExt();
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the MouseDown event of the Item control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseEventArgs" /> instance containing the event data.</param>
+        public void Item_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (CellClick != null)
+            {
+                CellClick(this, new DataGridViewEventArgs()
+                {
+                    CellControl = this,
+                    CellIndex =int.Parse((sender as Control).Tag.ToString())
+                });
+            }
+        }
+
+        /// <summary>
+        /// 设置选中状态，通常为设置颜色即可
+        /// </summary>
+        /// <param name="blnSelected">是否选中</param>
+        public void SetSelect(bool blnSelected)
+        {
+            if (blnSelected)
+            {
+                this.BackColor = Color.FromArgb(255, 247, 245);
+            }
+            else
+            {
+                this.BackColor = Color.Transparent;
+            }
+        }
+
+        /// <summary>
+        /// Reloads the cells.
+        /// </summary>
+        public void ReloadCells()
+        {
+            try
+            {
+               
+                this.panCells.Controls.Clear();
+                this.panCells.ColumnStyles.Clear();
+
+                int intColumnsCount = Columns.Count();
+                if (Columns != null && intColumnsCount > 0)
+                {
+                    if (IsShowCheckBox)
+                    {
+                        intColumnsCount++;
+                    }
+                    this.panCells.ColumnCount = intColumnsCount;
+                    for (int i = 0; i < intColumnsCount; i++)
+                    {
+                        Control c = null;
+                        if (i == 0 && IsShowCheckBox)
+                        {
+                            this.panCells.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(SizeType.Absolute, 30F));
+
+                            UCCheckBox box = new UCCheckBox();
+                            box.Name = "check";
+                            box.TextValue = "";
+                            box.Size = new Size(30, 30);
+                            box.Dock = DockStyle.Fill;
+                            box.CheckedChangeEvent += (a, b) =>
+                            {
+                                IsChecked = box.Checked;
+                                if (CheckBoxChangeEvent != null)
+                                {
+                                    CheckBoxChangeEvent(a, new DataGridViewEventArgs()
+                                    {
+                                        CellControl = box,
+                                        CellIndex = 0
+                                    });
+                                }
+                            };
+                            c = box;
+                        }
+                        else
+                        {
+                            var item = Columns[i - (IsShowCheckBox ? 1 : 0)];
+                            this.panCells.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(item.WidthType, item.Width));
+
+                            Label lbl = new Label();
+                            lbl.Tag = i - (IsShowCheckBox ? 1 : 0);
+                            lbl.Name = "lbl_" + item.DataField;
+                            lbl.Font = new Font("微软雅黑", 12);
+                            lbl.ForeColor = Color.Black;
+                            lbl.AutoSize = false;
+                            lbl.Dock = DockStyle.Fill;
+                            lbl.TextAlign = item.TextAlign;
+                            lbl.MouseDown += (a, b) =>
+                            {
+                                Item_MouseDown(a, b);
+                            };
+                            c = lbl;
+                        }
+                        this.panCells.Controls.Add(c, i, 0);
+                    }
+
+                }
+            }
+            finally
+            {
+                ControlHelper.FreezeControl(this, false);
+            }
+        }
+    }
+}
